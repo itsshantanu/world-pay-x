00abe4c6e9d27d17d8217b857931eb765899e2e9
Vipul tanwar <vipul@GKL0250.local>
2025-09-28 03:34:39 +0530

    rainbow

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -1,7 +1,15 @@
+'use client';
+
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { WalletProvider } from "../contexts/WalletContext";
-// import { Web3ModalProvider } from "../lib/wagmi";
+import { WagmiConfig, http } from 'wagmi';
+import { mainnet, sepolia } from 'wagmi/chains';
+import { RainbowKitProvider, getDefaultConfig } from '@rainbow-me/rainbowkit';
+import '@rainbow-me/rainbowkit/styles.css';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -13,10 +21,17 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
-export const metadata = {
-  title: "WorldPayX",
-  description: "Crypto-native subscription payments with yield",
-};
+
+const config = getDefaultConfig({
+  appName: 'WorldPayX',
+  projectId: 'd1ae274e5117a230816fc612d265e2da', // replace with one from cloud.walletconnect.com
+  chains: [sepolia, mainnet],
+  transports: {
+    [sepolia.id]: http(),
+    [mainnet.id]: http(),
+  },
+  ssr: true,
+});
 
 export default function RootLayout({ children }) {
   return (
@@ -24,9 +39,15 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <WalletProvider>
-          {children}
-        </WalletProvider>
+        <WagmiConfig config={config}>
+          <QueryClientProvider client={queryClient}>
+            <RainbowKitProvider>
+              <WalletProvider>
+                {children}
+              </WalletProvider>
+            </RainbowKitProvider>
+          </QueryClientProvider>
+        </WagmiConfig>
       </body>
     </html>
   );
